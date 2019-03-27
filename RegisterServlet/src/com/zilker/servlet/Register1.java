ab0649c9013fce16a3218aa23d7dc6c59fe29d2e
sruthiviswanathan <31382544+sruthiviswanathan@users.noreply.github.com>
2019-03-27 11:16:57 +0530

    Servlet Task

---
+++ RegisterServlet/src/com/zilker/servlet/Register1.java
@@ -0,0 +1,79 @@
+package com.zilker.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Register1
+ */
+@WebServlet("/Register1")
+public class Register1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Register1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		String n = request.getParameter("userName");
+		String p = request.getParameter("userPass");
+		String e = request.getParameter("userEmail");
+		String c = request.getParameter("userCountry");
+
+		Connection conn = null;
+
+		try {
+		
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+	
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet","root","root@123");
+
+			PreparedStatement ps = conn.prepareStatement("insert into REGISTERUSER values(?,?,?,?)");
+
+			ps.setString(1, n);
+			ps.setString(2, p);
+			ps.setString(3, e);
+			ps.setString(4, c);
+
+			int i = ps.executeUpdate();
+			if (i > 0)
+				out.print("You are successfully registered...");
+
+		} catch (Exception e2) {
+			System.out.println(e2);
+		}
+
+		out.close();
+	}
+
+	}
+
+
